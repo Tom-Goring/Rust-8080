86255b2ce0b42464dbd05e08d4a84e6c29a56fb2
Tom Goring <tgoring97@gmail.com>
2019-10-11 18:25:40 +0100

    Add mainloop in main (now the real bugfixing starts)

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ mod machine;
 mod screen;
 
 use std::fs::File;
-use std::io::prelude::*;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -29,9 +28,63 @@ fn main() -> Result<(), String> {
 
     let rom = File::open("./ROMS/invaders").unwrap();
     let rom_bytes: Vec<u8> = rom.bytes().map(|x| x.unwrap()).collect();
+    cpu.memory.load(0x0000, &rom_bytes);
 
+    loop {
+        cpu.tick(&mut machine);
+        let framebuffer = cpu.memory.view(0x2400, 0x3FFF);
 
-    pause();
+        screen.clear();
+        for y in 0..224 {
+            let line = &framebuffer[(32 * y)..(32 * y + 32)];
+            for (x, px) in line.iter().enumerate() {
+                for b in 0..8 {
+                    if px & (1 << b) != 0 {
+                        screen.draw(y as i16, 256 - (8 * x + b) as i16, 255)?;
+                    }
+                }
+            }
+        }
+        screen.canvas.present();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::KeyDown {
+                    keycode: Some(keycode),
+                    ..
+                } => match keycode {
+                    Keycode::Escape => return Ok(()),
+                    Keycode::A => machine.press_key(Keys::Left2),
+                    Keycode::D => machine.press_key(Keys::Right2),
+                    Keycode::Left => machine.press_key(Keys::Left1),
+                    Keycode::Right => machine.press_key(Keys::Right1),
+                    Keycode::Space => machine.press_key(Keys::Shoot1),
+                    Keycode::W => machine.press_key(Keys::Shoot2),
+                    Keycode::C => machine.release_key(Keys::Coin),
+                    Keycode::Num1 => machine.press_key(Keys::Start1),
+                    Keycode::Num2 => machine.press_key(Keys::Start2),
+                    _ => (),
+                },
+                Event::KeyUp {
+                    keycode: Some(keycode),
+                    ..
+                } => match keycode {
+                    Keycode::A => machine.release_key(Keys::Left2),
+                    Keycode::D => machine.release_key(Keys::Right2),
+                    Keycode::Left => machine.release_key(Keys::Left1),
+                    Keycode::Right => machine.release_key(Keys::Right1),
+                    Keycode::Space => machine.release_key(Keys::Shoot1),
+                    Keycode::W => machine.release_key(Keys::Shoot2),
+                    Keycode::C => machine.press_key(Keys::Coin),
+                    Keycode::Num1 => machine.release_key(Keys::Start1),
+                    Keycode::Num2 => machine.release_key(Keys::Start2),
+                    _ => (),
+                },
+                _ => {}
+            }
+        }
+        pause();
+    }
 
     Ok(())
 }
